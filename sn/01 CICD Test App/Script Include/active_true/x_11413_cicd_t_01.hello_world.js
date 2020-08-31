5bac719f48c5b7fa3787bfa221de4db89d09e4e8
System Administrator <boris_moers@rcomext.com>
2020-08-31 10:11:34 +0200

    Changes by 'System Administrator' on '01 app creation' -- Build #1

---
+++ sn/01 CICD Test App/Script Include/active_true/x_11413_cicd_t_01.hello_world.js
@@ -0,0 +1,16 @@
+/* 
+ * Application : 01 CICD Test App
+ * ClassName   : sys_script_include
+ * Created On  : 2020-08-31 08:08:00
+ * Created By  : admin
+ * Updated On  : 2020-08-31 08:08:00
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=d3ab28d0db8f1410fcf417803996190f
+ */
+var hello_world = Class.create();
+hello_world.prototype = {
+    initialize: function() {
+    },
+
+    type: 'hello_world'
+};
