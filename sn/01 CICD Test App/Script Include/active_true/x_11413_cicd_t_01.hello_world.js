655cfa7310051065e69e0e7a7d6457f01f74abef
System Administrator <boris_moers@rcomext.com>
2020-09-02 12:24:52 +0200

    Changes by 'System Administrator' on '01 app update 11' -- Build #1

--- sn/01 CICD Test App/Script Include/active_true/x_11413_cicd_t_01.hello_world.js
+++ sn/01 CICD Test App/Script Include/active_true/x_11413_cicd_t_01.hello_world.js
@@ -3,14 +3,14 @@
  * ClassName   : sys_script_include
  * Created On  : 2020-08-31 12:33:26
  * Created By  : admin
- * Updated On  : 2020-09-02 08:21:48
+ * Updated On  : 2020-09-02 10:21:38
  * Updated By  : admin
  * URL         : /sys_script_include.do?sys_id=ce98ad14db035410fcf41780399619e0
  */
 var hello_world = Class.create();
 hello_world.prototype = {
     initialize: function() {
-		// change on source 2
+		// change on source 3
 		return false;
     },
 
